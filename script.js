d1471e2c2e9e2fa807dfcd02f9b2a3a603353f1f
Lucas Heber <lucas.heber07@gmail.com>
2020-05-02 11:18:04 -0300

    [up] product

--- script.js
+++ script.js
@@ -8,6 +8,17 @@ $(document).ready(function() {
         $(this).addClass('active');
     });
 
+    $(".add-row").click(function() {
+        var markup = `<tr><td><input placeholder="preencha o titulo do atributo" class="form-control" type="text" name="" id=""></td>
+        <td><input  placeholder="preencha a informação atributo" class="form-control" type="text" name="" id=""></td>
+        <td><input onclick="$(this).closest('tr').remove();" type="button" value="Remover"></td></tr>`;
+        $("table tbody").append(markup);
+    });
+
+    // Find and remove selected table rows
+    $(".delete-row").click(function() {
+        $(this).parents("tr").remove();
+    });
 });
 
 function readURL(input) {
@@ -23,10 +34,10 @@ function readURL(input) {
             reader.onload = function(event) {
                 images = `<div class="form-group">
                                 <img class="pl-2" src="${event.target.result}" width="70" height="50">
-                                <button role="button" class="float-right pr-2 btn-danger btn-sm">Remover Foto</button> 
-                                <button role="button" class="float-right btn-primary btn-sm">Foto Capa</button>
-                         </div>
-                         <hr>`
+                                <input onclick="$(this).closest('div').remove();" type="button" class="float-right pr-2 btn-danger btn-sm" value="Remover">
+                                <input type="button" class="float-right btn-primary btn-sm" value="Foto Capa">
+                                <hr>
+                         </div>`
                 $(".div-images").append(images);
             }
 
@@ -35,3 +46,56 @@ function readURL(input) {
 
     }
 }
+
+/** VUE JS */
+var app = new Vue({
+    el: '#app',
+    data: {
+        pergunta: "",
+        resposta: "[Escreva uma resposta para o cliente]",
+        palavras_chave: "",
+        resposta_automatica: "",
+        arr_respostas: []
+    },
+    methods: {
+        remove_acentos(str) {
+            return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+        },
+        verificar_pergunta() {
+            let pergunta = this.remove_acentos(this.pergunta);
+
+            this.resposta = "";
+
+            for (i_r = 0; i_r < this.arr_respostas.length; i_r++) {
+
+                let r = this.arr_respostas[i_r];
+
+                for (i = 0; i < r.palavras_chave.length; i++) {
+                    let p = r.palavras_chave[i];
+
+                    if (pergunta == p || pergunta.includes(" " + p + " ") || pergunta.includes(" " + p + "?") || pergunta.includes(p + " ")) {
+                        this.resposta += r.resposta + "<br><br>";
+                        break;
+                    }
+                }
+            };
+
+            if (this.resposta == "") {
+                this.resposta = "[Escreva uma resposta para o cliente]";
+            }
+        },
+        adicionar_resposta_automatica() {
+            let palavras_chave = this.remove_acentos(this.palavras_chave);
+            palavras_chave = palavras_chave.split(' ');
+
+            this.arr_respostas.push({
+                palavras_chave: palavras_chave,
+                resposta: this.resposta_automatica
+            });
+
+            this.palavras_chave = "";
+            this.resposta_automatica = ""
+
+        }
+    }
+})
