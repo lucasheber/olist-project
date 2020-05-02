86fba0c8214a42969d8ab2d93ea96ed91abc07f5
Lucas Heber <lucas.heber07@gmail.com>
2020-05-02 00:55:07 -0300

    [up] index, [add] produto page

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ $(document).ready(function() {
     });
 
 });
+
+function readURL(input) {
+
+    if (input.files && input.files[0]) {
+        var filesAmount = input.files.length;
+
+        let images = ""
+
+        for (i = 0; i < filesAmount; i++) {
+            var reader = new FileReader();
+
+            reader.onload = function(event) {
+                images = `<div class="form-group">
+                                <img class="pl-2" src="${event.target.result}" width="70" height="50">
+                                <button role="button" class="float-right pr-2 btn-danger btn-sm">Remover Foto</button> 
+                                <button role="button" class="float-right btn-primary btn-sm">Foto Capa</button>
+                         </div>
+                         <hr>`
+                $(".div-images").append(images);
+            }
+
+            reader.readAsDataURL(input.files[i]);
+        }
+
+    }
+}
